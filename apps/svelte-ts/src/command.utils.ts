953a907e73abd3e09b91fca87dfe7828e7ada9d1
Oskar Hane <oh@oskarhane.com>
2022-10-12 14:19:18 +0200

    More help info in svelte app

--- apps/svelte-ts/src/command.utils.ts
+++ apps/svelte-ts/src/command.utils.ts
@@ -3,7 +3,9 @@ import type { GenericResult } from "./frames/Generic.types";
 export function runCommand(cmd: string): Promise<GenericResult> {
   if (cmd.trim() === ":help") {
     return new Promise((res) =>
-      res({ data: "- Cmd / Ctrl + Enter to run command or query" })
+      res({
+        data: "- Cmd / Ctrl + Enter to run command or query\n- Cmd / Ctrl + Up / Down to step in history"
+      })
     );
   }
 }
