37252c79abe81418789432b09928fd80736eb4a5
Oskar Hane <oh@oskarhane.com>
2023-01-19 12:16:53 +0100

    Format Cypher results nicely in the Svelte demo app

---
+++ .changeset/late-mugs-hide.md
@@ -0,0 +1,5 @@
+---
+"svelte-ts": patch
+---
+
+Format Cypher results nicely in the Svelte demo app
--- apps/svelte-ts/src/frames/Cypher.svelte
+++ apps/svelte-ts/src/frames/Cypher.svelte
@@ -20,13 +20,91 @@
  */
 -->
 <script lang="ts">
-  import type { QueryResult } from "neo4j-driver";
+  import { Node, isInt, type QueryResult } from "neo4j-driver";
   export let result: QueryResult;
+  const recordObjs: { keys: string[]; rows: any[] } = result.records.reduce(
+    (all, r, index) => {
+      const keys = r.keys;
+      if (index === 0) {
+        all.keys = [...keys];
+      }
+      all.rows.push(
+        keys.map((key) => JSON.stringify(formatValue(r.get(key)), null, 2))
+      );
+      return all;
+    },
+    { keys: [], rows: [] }
+  );
+  function formatValue(x: any) {
+    if (isInt(x)) {
+      return x.toInt();
+    }
+    if (Number(x) === x) {
+      // Not loving that we get quotes around floats,
+      // but it's the only way to differenciate ints from floats
+      // without having to write our own JSON.stringify
+      return Number.isInteger(x) ? x.toFixed(1) : x;
+    }
+    if (typeof x === "string" && !x.startsWith("{")) {
+      return x;
+    }
+    // JSON strings, do not quote
+    if (typeof x === "string") {
+      return JSON.parse(x);
+    }
+
+    if (x === null) {
+      return x;
+    }
+
+    if (Array.isArray(x)) {
+      return x.map((v) => formatValue(v));
+      return x;
+    }
+
+    if (typeof x === "object") {
+      let out = {};
+      Object.keys(x).forEach((key) => {
+        out[key] = formatValue(x[key]);
+      });
+      return out;
+    }
+
+    return x;
+  }
 </script>
 
-<pre>{JSON.stringify(result.records, null, 2)}</pre>
+<table>
+  <thead
+    ><tr>
+      {#each recordObjs.keys as key}
+        <td>{key}</td>
+      {/each}
+    </tr></thead
+  >
+  <tbody>
+    {#each recordObjs.rows as row}
+      <tr>
+        {#each row as col}
+          <td><pre>{col}</pre></td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
 
 <style>
+  table {
+    width: 100%;
+  }
+  thead td {
+    border-bottom: 1px solid #ccc;
+    font-size: 1rem;
+  }
+
+  td {
+    min-width: 200px;
+  }
   pre {
     background-color: #fafafa;
     border-radius: 6px;
