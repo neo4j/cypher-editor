2c37f186e12eeeb14de653623b892bc2bf796669
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-03-07 15:54:11 +0100

    fix incorrect import

--- packages/editor-support/src/util/createCypherLexer.js
+++ packages/editor-support/src/util/createCypherLexer.js
@@ -1,5 +1,5 @@
 import antrl4 from "@neo4j-cypher/antlr4-browser";
-import { CypherLexer } from "../_generated.simple/CypherLexer";
+import { CypherLexer } from "@neo4j-cypher/antlr4";
 
 export const createCypherLexer = (input) => {
   const chars = new antrl4.InputStream(input);
